683db21abd687bda1773016ee8563565a1e7bca8
Carl Lubojanski <lubojcar@students.zhaw.ch>
2020-10-17 11:11:33 +0200

    Added methods findby: plz, status, rating in the UserRepository

--- src/main/java/ch/zhaw/pm3/helpy/repository/UserRepository.java
+++ src/main/java/ch/zhaw/pm3/helpy/repository/UserRepository.java
@@ -4,7 +4,18 @@ import ch.zhaw.pm3.helpy.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
+
 public interface UserRepository extends JpaRepository<User, Long> {
     @Query(value = "select * from User as u where u.email=?1", nativeQuery = true)
     User findUserByName(String username);
+
+    @Query(value = "select * from User where plz=?1", nativeQuery = true)
+    List<User> findUsersByPlz(int plz);
+
+    @Query(value = "select * from User where status=?1", nativeQuery = true)
+    List<User> findUsersByStatus(String status);
+
+    @Query(value = "select * from Helper where ratings=?1", nativeQuery = true)
+    List<User> findUsersByRating(int rating);
 }
