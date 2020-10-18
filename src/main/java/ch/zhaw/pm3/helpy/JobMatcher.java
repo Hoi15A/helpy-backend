f24c92088dec24e5be16df3a03b4c3d71b197971
meletlea <meletlea@students.zhaw.ch>
2020-10-18 20:36:49 +0200

    #18 implement getHelperNearHelpseeker

--- src/main/java/ch/zhaw/pm3/helpy/JobMatcher.java
+++ src/main/java/ch/zhaw/pm3/helpy/JobMatcher.java
@@ -2,12 +2,14 @@ package ch.zhaw.pm3.helpy;
 
 import ch.zhaw.pm3.helpy.model.Helper;
 import ch.zhaw.pm3.helpy.model.Job;
+import ch.zhaw.pm3.helpy.model.User;
 import ch.zhaw.pm3.helpy.repository.UserRepository;
 import com.sun.istack.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Provides an appropiate list of Helper.
@@ -37,7 +39,17 @@ public class JobMatcher {
     }
 
     private List<Helper> getHelperNearHelpseeker() {
-        return new ArrayList<>();
+        List<User> users = new ArrayList<>();
+        int jobPlz = job.getAuthor().getPlz(); // PLZ in switzerland is a 4 digit number
+        int plzRadius = 3;
+        for (int i = -plzRadius; i < plzRadius; i++) {
+            int plz = i + jobPlz;
+            users.addAll(userRepository.findUsersByPlz(plz));
+        }
+
+        return users.stream().filter(Helper.class::isInstance)
+                .map(Helper.class::cast)
+                .collect(Collectors.toList());
     }
 
     private List<Helper> match(List<Helper> potentialHelper) {
