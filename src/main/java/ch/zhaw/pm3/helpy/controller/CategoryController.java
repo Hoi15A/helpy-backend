f3fb54b6e687a21a2f0f42772a1488c01954c0a9
Carl Lubojanski <lubojcar@students.zhaw.ch>
2020-10-19 08:43:48 +0200

    CategoryController implementiert

--- src/main/java/ch/zhaw/pm3/helpy/controller/CategoryController.java
+++ src/main/java/ch/zhaw/pm3/helpy/controller/CategoryController.java
@@ -1,42 +1,60 @@
 package ch.zhaw.pm3.helpy.controller;
 
+import ch.zhaw.pm3.helpy.model.Category;
+import ch.zhaw.pm3.helpy.repository.CategoryRepository;
+import com.google.gson.Gson;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("api/category")
 public class CategoryController {
+    @Autowired
+    CategoryRepository categoryRepository;
 
-    //+ getCategories(): ResponseEntity
-    //+ getCategorieByName(name: String): ResponseEntity
-    //+ getRelatedCategories(category: Category): ResponseEntity
+    private Gson gson = new Gson();
 
     @GetMapping("all")
-    public ResponseEntity<List<String>> getCategories() {
-        //todo
-        List<String> categories = new ArrayList<>();
-        categories.add("Sprache");
-        return ResponseEntity.badRequest().body(categories);
+    public ResponseEntity<List<Category>> getCategories() {
+        return ResponseEntity.badRequest().body(categoryRepository.findAll());
+    }
+
+    @PostMapping("add")
+    public ResponseEntity<Category> createCategory(@RequestBody final String body) {
+        Category category = gson.fromJson(body, Category.class);
+        categoryRepository.save(category);
+        return ResponseEntity.ok(category);
+    }
+
+    @PutMapping("update")
+    public ResponseEntity<Category> updateCategory(@RequestBody final String body) {
+        Category newCat = gson.fromJson(body, Category.class);
+        Category oldCat = categoryRepository.findCategoryByName(newCat.getName());
+        if (oldCat == null) return ResponseEntity.notFound().build();
+        categoryRepository.save(newCat);
+        return ResponseEntity.ok(newCat);
+    }
+
+    @DeleteMapping("remove/{category}")
+    public ResponseEntity<Category> removeCategory(@PathVariable(name = "category") final String name) {
+        Category category = categoryRepository.findCategoryByName(name);
+        if (category == null) return ResponseEntity.notFound().build();
+        categoryRepository.delete(category);
+        return ResponseEntity.ok(category);
     }
 
     @GetMapping("{category}")
-    public ResponseEntity<String> getCategoryByName(@PathVariable("category") final String category) {
-        //todo
-        return ResponseEntity.badRequest().body("Not Implemented");
+    public ResponseEntity<Category> getCategoryByName(@PathVariable("category") final String name) {
+        Category category = categoryRepository.findCategoryByName(name);
+        return category == null ? ResponseEntity.notFound().build() : ResponseEntity.ok(category);
     }
 
     @GetMapping("related/{category}")
-    public ResponseEntity<List<String>> getRelatedCategories(@PathVariable("category") final String category) {
-        //todo
-        List<String> relatedCategories = new ArrayList<>();
-        relatedCategories.add("SBB");
-        return ResponseEntity.badRequest().body(relatedCategories);
+    public ResponseEntity<List<Category>> getRelatedCategories(@PathVariable("category") final String category) {
+        return ResponseEntity.ok(categoryRepository.findRelatedCategories(category));
     }
 }
 
