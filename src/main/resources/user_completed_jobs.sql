223840e82caa156534b9ea87c2a0160cdb635a63
meletlea <meletlea@students.zhaw.ch>
2020-10-21 18:25:29 +0200

    added user_completed_jobs and user_tasks #29

---
+++ src/main/resources/user_completed_jobs.sql
@@ -0,0 +1,8 @@
+insert into User_Completed_Jobs (helper_email, completed_jobs_id) values ('rlavigne0@virginia.edu', 3);
+insert into User_Completed_Jobs (helper_email, completed_jobs_id) values ('rsoots2@tripod.com', 4);
+insert into User_Completed_Jobs (helper_email, completed_jobs_id) values ('nanton4@amazon.co.jp', 9);
+insert into User_Completed_Jobs (helper_email, completed_jobs_id) values ('mpetkovic6@friendfeed.com', 14);
+insert into User_Completed_Jobs (helper_email, completed_jobs_id) values ('gvitall8@booking.com', 22);
+insert into User_Completed_Jobs (helper_email, completed_jobs_id) values ('sshieldona@acquirethisname.com', 29);
+insert into User_Completed_Jobs (helper_email, completed_jobs_id) values ('tfleischerc@chronoengine.com', 33);
+insert into User_Completed_Jobs (helper_email, completed_jobs_id) values ('cmewe@jimdo.com', 35);
