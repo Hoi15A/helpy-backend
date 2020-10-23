e660f1015093649e4655476601abfbba9cc6e799
Carl Lubojanski <lubojcar@students.zhaw.ch>
2020-10-23 10:45:42 +0200

    Added constraints to the User

--- src/main/java/ch/zhaw/pm3/helpy/model/User.java
+++ src/main/java/ch/zhaw/pm3/helpy/model/User.java
@@ -7,23 +7,34 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.validation.constraints.*;
 import java.time.LocalDate;
 
 @Entity
 public class User {
-
     @Id
+    @Email(message = "You have to enter a valid email address")
     private String email;
+    @NotBlank(message = "You have to enter your first name")
     private String firstname;
+    @NotBlank(message = "You have to enter your last name")
     private String lastname;
+    @NotNull(message = "You have to enter your sex")
     private char sex;
+    @NotNull(message = "You have to enter the postcode")
     private int plz;
+    @NotNull(message = "You have to enter your birthdate")
     private LocalDate birthdate;
     @JsonIgnore
+    @NotBlank(message = "You have to provide a password")
+    @Size(message = "Your password has to between 8 and 32 characters", min = 8, max = 32)
     private String password;
     @Column(columnDefinition = "LONGTEXT")
+    @NotEmpty(message = "You have to enter your biographie")
     private String biographie;
+    @NotNull(message = "You have to enter a status")
     private UserStatus status;
+    @NotNull(message = "You have to enter a permission type")
     private Permission permission;
 
     public User() {
