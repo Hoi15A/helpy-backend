1062a643785fa5d1baa15920d7d6027a5150c8d9
Carl Lubojanski <lubojcar@students.zhaw.ch>
2020-10-24 16:23:52 +0200

    added missing diamond operator in ApiExceptionHandler

--- src/main/java/ch/zhaw/pm3/helpy/exception/ApiExceptionHandler.java
+++ src/main/java/ch/zhaw/pm3/helpy/exception/ApiExceptionHandler.java
@@ -3,6 +3,8 @@ package ch.zhaw.pm3.helpy.exception;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.http.converter.HttpMessageNotWritableException;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -29,7 +31,7 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
         List<String> details = new ArrayList<>();
         details.add(ex.getLocalizedMessage());
         ErrorResponse error = new ErrorResponse("Record Not Found", details);
-        return new ResponseEntity(error, HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
 
     @Override
