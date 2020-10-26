9e71003a7f3312668822563ff1d0fd9372793053
meletlea <meletlea@students.zhaw.ch>
2020-10-26 08:50:16 +0100

    remove unused import in TestCategoryController

--- src/test/java/ch/zhaw/pm3/helpy/controller/TestCategoryController.java
+++ src/test/java/ch/zhaw/pm3/helpy/controller/TestCategoryController.java
@@ -12,7 +12,6 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.mock;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
