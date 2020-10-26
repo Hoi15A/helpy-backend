5fdc50316fe27b35091fd26fd342aeaa2e44bf80
meletlea <meletlea@students.zhaw.ch>
2020-10-26 18:21:14 +0100

    remove unnecessary statements in deleteJobById in JobService

--- src/main/java/ch/zhaw/pm3/helpy/service/JobService.java
+++ src/main/java/ch/zhaw/pm3/helpy/service/JobService.java
@@ -195,19 +195,18 @@ public class JobService {
         if (optionalJob.isEmpty()) throw new RecordNotFoundException(String.valueOf(id));
         Job job = optionalJob.get();
 
+        // resolve author constrain
         Helpseeker helpseeker = job.getAuthor();
         helpseeker.getTasks().remove(job);
 
+        // resolve helper constrain
         Helper helper = job.getMatchedHelper();
         if (helper != null) {
             helper.getCompletedJobs().remove(job);
-            userRepository.save(helper);
         }
 
         job.setMatchedHelper(null);
         job.setAuthor(null);
-        jobRepository.save(job);
-        userRepository.save(helpseeker);
         jobRepository.delete(job);
     }
 
