ec97cab8c66e1fc80d4038fd1dcf946ae62ad05d
Carl Lubojanski <carl.lubojanski@outlook.com>
2020-11-02 21:41:07 +0100

    added javadoc for Weekdays #27

--- src/main/java/ch/zhaw/pm3/helpy/constant/Weekdays.java
+++ src/main/java/ch/zhaw/pm3/helpy/constant/Weekdays.java
@@ -1,5 +1,8 @@
 package ch.zhaw.pm3.helpy.constant;
 
+/**
+ * Enumeration of weekdays for the {@link ch.zhaw.pm3.helpy.model.Job}.
+ */
 public enum Weekdays {
     MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY
 }
