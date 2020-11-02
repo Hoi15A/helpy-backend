8ba939f6def457fad9da517114515d7baea4c28d
Carl Lubojanski <carl.lubojanski@outlook.com>
2020-11-02 21:28:35 +0100

    Added javadoc to RecordNotFoundException #27

--- src/main/java/ch/zhaw/pm3/helpy/exception/RecordNotFoundException.java
+++ src/main/java/ch/zhaw/pm3/helpy/exception/RecordNotFoundException.java
@@ -1,6 +1,13 @@
 package ch.zhaw.pm3.helpy.exception;
 
+/**
+ * Is thrown if a record could not be found in the database.
+ */
 public class RecordNotFoundException extends RuntimeException {
+    /**
+     * Creates a new record not found exception instance.
+     * @param message String
+     */
     public RecordNotFoundException(String message) {
         super(message);
     }
