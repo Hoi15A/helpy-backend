3bcba95404c281d3587e4205fc5a12794b7b6f60
Carl Lubojanski <carl.lubojanski@outlook.com>
2020-11-02 21:07:20 +0100

    added javadoc to JobController #30

--- src/main/java/ch/zhaw/pm3/helpy/controller/JobController.java
+++ src/main/java/ch/zhaw/pm3/helpy/controller/JobController.java
@@ -13,85 +13,164 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.*;
 
+/**
+ * This class is for the manipulation of the {@link Job} model.
+ */
 @RestController
 @RequestMapping("api/job")
 public class JobController {
 
     private final JobService jobService;
 
+    /**
+     * Autowires the job service for the job management.
+     * @param jobService {@link JobService}
+     */
     public JobController(JobService jobService) {
         this.jobService = jobService;
     }
 
+    /**
+     * Returns all jobs from the database as json.
+     * @return ResponseEntity<List<Job>>
+     */
     @GetMapping("all")
     public ResponseEntity<List<Job>> getJobs() {
         return ResponseEntity.ok(jobService.getAllJobs());
     }
 
+    /**
+     * Takes a {@link Job} and saves it in the database.
+     * The same {@link Job} will be returned after creation.
+     * @param job {@link Job}
+     * @return ResponseEntity<Job>
+     */
     @PostMapping(path = "add", consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Job> createJob(@Valid @RequestBody final Job job) {
         jobService.createJob(job);
         return ResponseEntity.ok(job);
     }
 
+    /**
+     * Takes a job id and deletes the object from the database.
+     * @param id long
+     * @return ResponseEntity<Object>
+     */
     @DeleteMapping("remove/{id}")
     public ResponseEntity<Object> removeJob(@PathVariable("id") final long id) {
         jobService.deleteJobById(id);
         return ResponseEntity.ok().build();
     }
 
+    /**
+     * Takes a {@link Job} and updates the database entry with the same id.
+     * The saved {@link Job} will be returned after the update.
+     * @param job {@link Job}
+     * @return ResponseEntity<Job>
+     */
     @PutMapping(path = "update", consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Job> updateJob(@Valid @RequestBody final Job job) {
         jobService.updateJob(job);
         return ResponseEntity.ok(job);
     }
 
+    /**
+     * Takes a job id and returns the found {@link Job} object from the database.
+     * @param id long
+     * @return ResponseEntity<Job>
+     */
     @GetMapping("id/{id}")
     public ResponseEntity<Job> getJobById(@PathVariable("id") final long id) {
         return ResponseEntity.ok(jobService.getJobById(id));
     }
 
+    /**
+     * Takes a job id and returns a list of potential helpers.
+     * @param id long
+     * @return ResponseEntity<List<Helper>>
+     */
     @GetMapping("id/{id}/find-helper")
     public ResponseEntity<List<Helper>> findPotentialHelper(@PathVariable("id") final long id) {
         return ResponseEntity.ok(jobService.getPotentialHelpersForJob(id));
     }
 
+    /**
+     * Takes a job id as well as an helper email and adds it to the job.
+     * @param id long
+     * @param email string
+     * @return ResponseEntity<Job>
+     */
     @PutMapping("id/{id}/set-helper/{mail}")
     public ResponseEntity<Job> updateHelper(@PathVariable("id") final long id,
                                                      @PathVariable("mail") final String email) {
         return ResponseEntity.ok(jobService.addHelperForJob(id, email));
     }
 
+    /**
+     * Takes a job status and returns all jobs with the according status.
+     * @param status {@link JobStatus}
+     * @return ResponseEntity<List<Job>>
+     */
     @GetMapping("status/{status}")
     public ResponseEntity<List<Job>> getJobsByStatus(@PathVariable("status") final JobStatus status) {
         return ResponseEntity.ok(jobService.getJobsByStatus(status));
     }
 
+    /**
+     * Takes an email from the author and returns a list of jobs from the same author.
+     * @param email string
+     * @return ResponseEntity<List<Job>>
+     */
     @GetMapping("author/{author}")
     public ResponseEntity<List<Job>> getJobsByAuthor(@PathVariable("author") final String email) {
         return ResponseEntity.ok(jobService.getJobsByAuthor(email));
     }
 
+    /**
+     * Takes a category name and returns all jobs with the provided category name.
+     * @param category string
+     * @return ResponseEntity<List<Job>>
+     */
     @GetMapping("category/{category}")
     public ResponseEntity<List<Job>> getJobsByCategory(@PathVariable("category") final String category) {
         return ResponseEntity.ok(jobService.getJobsByCategory(category));
     }
 
+    /**
+     * Takes a {@link Category} array and returns all jobs with the provided categories.
+     * @param categories {@link Category}[]
+     * @return ResponseEntity<Set<Job>>
+     */
     @PostMapping("categories")
     public ResponseEntity<Set<Job>> getJobsByCategories(@Valid @RequestBody final Category[] categories) {
         return ResponseEntity.ok(jobService.getJobsByCategories(categories));
     }
 
+    /**
+     * Takes a tag name and returns all the jobs with the provided tag name.
+     * @param tag string
+     * @return ResponseEntity<List<Job>>
+     */
     @GetMapping("tag/{tag}")
     public ResponseEntity<List<Job>> getJobsByTag(@PathVariable("tag") final String tag) {
         return ResponseEntity.ok(jobService.getJobsByTag(tag));
     }
 
+    /**
+     * Takes a {@link Tag} array and returns all the Jobs with the provided tags.
+     * @param tags {@link Tag}[]
+     * @return ResponseEntity<Set<Job>>
+     */
     @PostMapping("tags")
     public ResponseEntity<Set<Job>> getJobsByTags(@Valid @RequestBody final Tag[] tags) {
         return ResponseEntity.ok(jobService.getJobsByTags(tags));
     }
 
+    /**
+     * Takes a date and returns all jobs with the same creation date.
+     * @param date string
+     * @return ResponseEntity<List<Job>>
+     */
     @GetMapping("date/{date}")
     public ResponseEntity<List<Job>> getJobsByDate(@PathVariable("date") final String date) {
         return ResponseEntity.ok(jobService.getJobsByDate(date));
