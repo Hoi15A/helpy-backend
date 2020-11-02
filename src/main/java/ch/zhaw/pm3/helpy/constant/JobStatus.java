7a2953845ada9c22cabafd8a395571cbd27846e5
Carl Lubojanski <carl.lubojanski@outlook.com>
2020-11-02 21:40:40 +0100

    added javadoc for JobStatus #27

--- src/main/java/ch/zhaw/pm3/helpy/constant/JobStatus.java
+++ src/main/java/ch/zhaw/pm3/helpy/constant/JobStatus.java
@@ -1,5 +1,8 @@
 package ch.zhaw.pm3.helpy.constant;
 
+/**
+ * Enumeration of different statuses for the {@link ch.zhaw.pm3.helpy.model.Job} class.
+ */
 public enum JobStatus {
     OPEN,
     IN_PROGRESS,
