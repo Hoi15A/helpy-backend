beb9520da08887843fda0ce4e80e6e111ecb2652
Carl Lubojanski <carl.lubojanski@outlook.com>
2020-11-02 21:35:03 +0100

    Added javadoc to Tag #27

--- src/main/java/ch/zhaw/pm3/helpy/model/category/Tag.java
+++ src/main/java/ch/zhaw/pm3/helpy/model/category/Tag.java
@@ -2,11 +2,22 @@ package ch.zhaw.pm3.helpy.model.category;
 
 import javax.persistence.Entity;
 
+/**
+ * Provides a tag representation.
+ */
 @Entity
 public class Tag extends Category {
+    /**
+     * Creates a new tag instance.
+     */
     public Tag() {
         //needed for JPA
     }
+
+    /**
+     * Creates a new tag instance.
+     * @param name String
+     */
     public Tag(String name) {
         super(name);
     }
