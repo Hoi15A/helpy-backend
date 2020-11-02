351134cbc6feb5c064017d13b5df3c057ebd2158
Carl Lubojanski <carl.lubojanski@outlook.com>
2020-11-02 21:40:49 +0100

    added javadoc for Permission #27

--- src/main/java/ch/zhaw/pm3/helpy/constant/Permission.java
+++ src/main/java/ch/zhaw/pm3/helpy/constant/Permission.java
@@ -1,5 +1,8 @@
 package ch.zhaw.pm3.helpy.constant;
 
+/**
+ * Enumeration of permissions for the {@link ch.zhaw.pm3.helpy.model.user.User}.
+ */
 public enum Permission {
     ADMINISTRATOR, MODERATOR, USER, GUEST
 }
