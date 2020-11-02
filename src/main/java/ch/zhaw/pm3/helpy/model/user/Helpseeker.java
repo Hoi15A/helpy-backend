51e29a36e892d3f11b81ba727fc9bae304cced9f
Carl Lubojanski <carl.lubojanski@outlook.com>
2020-11-02 21:35:29 +0100

    Added javadoc to Helpseeker #27

--- src/main/java/ch/zhaw/pm3/helpy/model/user/Helpseeker.java
+++ src/main/java/ch/zhaw/pm3/helpy/model/user/Helpseeker.java
@@ -7,12 +7,18 @@ import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 import java.util.List;
 
+/**
+ * Model class which holds the information for the Helpy help seeker.
+ */
 @Entity
 public class Helpseeker extends User {
     @OneToMany(mappedBy = "author")
     @JsonBackReference
     private List<Job> tasks;
 
+    /**
+     * Creates a new help seeker instance.
+     */
     public Helpseeker() {
         //needed for JPA
     }
