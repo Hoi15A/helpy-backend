e1965681092538bdc96a7d92ea7b1130633a5921
Carl Lubojanski <carl.lubojanski@outlook.com>
2020-11-02 21:28:13 +0100

    Added javadoc to RecordAlreadyExistsException #27

--- src/main/java/ch/zhaw/pm3/helpy/exception/RecordAlreadyExistsException.java
+++ src/main/java/ch/zhaw/pm3/helpy/exception/RecordAlreadyExistsException.java
@@ -1,6 +1,13 @@
 package ch.zhaw.pm3.helpy.exception;
 
+/**
+ * Is thrown if a record already exists in the database.
+ */
 public class RecordAlreadyExistsException extends RuntimeException {
+    /**
+     * Creates a new record already exists exception instance.
+     * @param message String
+     */
     public RecordAlreadyExistsException(String message) {
         super(message);
     }
