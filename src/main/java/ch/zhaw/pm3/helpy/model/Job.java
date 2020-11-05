fc3a58347c470d459cdfbfa17e6b8b8cad830849
meletlea <meletlea@students.zhaw.ch>
2020-11-05 11:33:28 +0100

    remove unnecessary lombok annotation #41

--- src/main/java/ch/zhaw/pm3/helpy/model/Job.java
+++ src/main/java/ch/zhaw/pm3/helpy/model/Job.java
@@ -9,7 +9,6 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
@@ -20,7 +19,6 @@ import java.util.List;
 /**
  * Model class which holds the information for the Helpy job.
  */
-@NoArgsConstructor
 @Data
 @Entity
 public class Job {
