b04f9e227c17701fe2c8591f7559f5241d36c517
pedernin <pedernin@students.zhaw.ch>
2020-11-30 00:29:29 +0100

    changed className to TagMatcher and implemented TagMatcher class #70

---
+++ src/main/java/ch/zhaw/pm3/helpy/matcher/strategy/TagMatcher.java
@@ -0,0 +1,27 @@
+package ch.zhaw.pm3.helpy.matcher.strategy;
+
+import ch.zhaw.pm3.helpy.constant.UserStatus;
+import ch.zhaw.pm3.helpy.model.job.Job;
+import ch.zhaw.pm3.helpy.model.user.User;
+
+import java.util.Collections;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class TagMatcher extends MatcherStrategy {
+
+    private Set<User> users;
+
+    @Override
+    public Set<User> getPotentialHelpers(Job job) {
+        users = getUserRepository().findUsersWithCategoriesAndTagsByStatus(UserStatus.ACTIVE);
+        return getHelpersWithMatchingTags(job);
+    }
+
+    private Set<User> getHelpersWithMatchingTags(Job job) {
+        return users.stream()
+                .filter(User::isWantsToHelpActive)
+                .filter(helper -> !Collections.disjoint(helper.getTags(), job.getTags()))
+                .collect(Collectors.toSet());
+    }
+}
