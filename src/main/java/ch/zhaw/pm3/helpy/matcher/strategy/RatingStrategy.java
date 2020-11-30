596397dc82904904db8a11893b44a33234ef6a8a
Carl Lubojanski <lubojcar@students.zhaw.ch>
2020-11-30 21:20:09 +0100

    Removed unused import
    Co-Authored-By: meletlea <3164+meletlea@users.noreply.github.zhaw.ch>

--- src/main/java/ch/zhaw/pm3/helpy/matcher/strategy/RatingStrategy.java
+++ src/main/java/ch/zhaw/pm3/helpy/matcher/strategy/RatingStrategy.java
@@ -2,10 +2,8 @@ package ch.zhaw.pm3.helpy.matcher.strategy;
 
 import ch.zhaw.pm3.helpy.model.job.Job;
 import ch.zhaw.pm3.helpy.model.user.User;
-import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Collection;
-import java.util.Set;
 
 public class RatingStrategy implements Strategy {
     @Override
