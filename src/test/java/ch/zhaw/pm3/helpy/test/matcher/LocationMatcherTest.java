5e3fc30de34eaac0ac314f5a29860fee4cce7a46
Carl Lubojanski <lubojcar@students.zhaw.ch>
2020-11-30 21:36:25 +0100

    Added LocationMatcherTest
    Co-Authored-By: meletlea <3164+meletlea@users.noreply.github.zhaw.ch>

---
+++ src/test/java/ch/zhaw/pm3/helpy/test/matcher/LocationMatcherTest.java
@@ -0,0 +1,58 @@
+package ch.zhaw.pm3.helpy.test.matcher;
+
+import ch.zhaw.pm3.helpy.matcher.strategy.LocationStrategy;
+import ch.zhaw.pm3.helpy.matcher.strategy.MatcherStrategy;
+import ch.zhaw.pm3.helpy.model.job.Job;
+import ch.zhaw.pm3.helpy.model.user.User;
+import ch.zhaw.pm3.helpy.repository.LocationRepository;
+import ch.zhaw.pm3.helpy.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.*;
+
+@SpringBootTest
+class LocationMatcherTest {
+
+    @Autowired
+    LocationRepository locationRepository;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Mock
+    Job job;
+
+    @Mock
+    User user;
+
+    MatcherStrategy locationStrategy = new LocationStrategy();
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+        locationStrategy.setLocationRepository(locationRepository);
+        locationStrategy.setUserRepository(userRepository);
+        when(job.getAuthor()).thenReturn(user);
+        when(job.getAuthor().getPlz()).thenReturn(8400);
+    }
+
+    @Test
+    void contextLoads() {
+        assertNotNull(locationRepository);
+    }
+
+    @Test
+    void testLocation() {
+        assertEquals(3, locationStrategy.getPotentialHelpers(job).size());
+        when(job.getAuthor().getPlz()).thenReturn(1723);
+        assertEquals(3, locationStrategy.getPotentialHelpers(job).size());
+    }
+
+}
