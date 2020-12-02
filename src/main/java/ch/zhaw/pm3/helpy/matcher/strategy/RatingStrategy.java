02868d02c7088a459a8dbeb798e34d2b8defb5de
Carl Lubojanski <lubojcar@students.zhaw.ch>
2020-12-02 09:26:17 +0100

    Implemented RatingMatcher #68

--- src/main/java/ch/zhaw/pm3/helpy/matcher/strategy/RatingStrategy.java
+++ src/main/java/ch/zhaw/pm3/helpy/matcher/strategy/RatingStrategy.java
@@ -4,10 +4,22 @@ import ch.zhaw.pm3.helpy.model.job.Job;
 import ch.zhaw.pm3.helpy.model.user.User;
 
 import java.util.Collection;
+import java.util.Comparator;
+import java.util.stream.Collectors;
 
-public class RatingStrategy implements Strategy {
+public class RatingStrategy extends MatcherStrategy {
     @Override
     public Collection<User> getPotentialHelpers(Job job) {
-        return null;
+        Comparator<User> comparator = (o1, o2) -> {
+            double u1 = (double) o1.getRatings().stream().mapToInt(Integer::valueOf).sum() / o1.getRatings().size();
+            double u2 = (double) o2.getRatings().stream().mapToInt(Integer::valueOf).sum() / o2.getRatings().size();
+            return Double.compare(u2, u1);
+        };
+
+        return getUserRepository().findAllByRating()
+                .stream()
+                .filter(User::isWantsToHelpActive)
+                .sorted(comparator)
+                .collect(Collectors.toList());
     }
 }
