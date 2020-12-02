5cc3edc25c9b5097b38e186df4ce629f79e08910
Carl Lubojanski <lubojcar@students.zhaw.ch>
2020-12-02 09:31:43 +0100

    Refactored comparator in RatingStrategy #68

--- src/main/java/ch/zhaw/pm3/helpy/matcher/strategy/RatingStrategy.java
+++ src/main/java/ch/zhaw/pm3/helpy/matcher/strategy/RatingStrategy.java
@@ -11,8 +11,8 @@ public class RatingStrategy extends MatcherStrategy {
     @Override
     public Collection<User> getPotentialHelpers(Job job) {
         Comparator<User> comparator = (o1, o2) -> {
-            double u1 = (double) o1.getRatings().stream().mapToInt(Integer::valueOf).sum() / o1.getRatings().size();
-            double u2 = (double) o2.getRatings().stream().mapToInt(Integer::valueOf).sum() / o2.getRatings().size();
+            double u1 = (double) o1.getRatings().stream().reduce(0, Integer::sum) / o1.getRatings().size();
+            double u2 = (double) o2.getRatings().stream().reduce(0, Integer::sum) / o2.getRatings().size();
             return Double.compare(u2, u1);
         };
 
