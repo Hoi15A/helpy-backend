77f0c075cb80977e3443621b6c5209019e6888b7
pedernin <pedernin@students.zhaw.ch>
2020-12-03 21:46:49 +0100

    Updated UserCategoryStrategy #70

--- src/main/java/ch/zhaw/pm3/helpy/matcher/strategy/CategoryStrategy.java
+++ src/main/java/ch/zhaw/pm3/helpy/matcher/strategy/CategoryStrategy.java
@@ -1,22 +1,39 @@
 package ch.zhaw.pm3.helpy.matcher.strategy;
 
+import ch.zhaw.pm3.helpy.model.category.Category;
 import ch.zhaw.pm3.helpy.model.job.Job;
 import ch.zhaw.pm3.helpy.model.user.User;
 import ch.zhaw.pm3.helpy.model.user.UserStatus;
 
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class CategoryStrategy implements Strategy {
 
     @Override
     public Collection<User> filterPotentialHelpers(Job job, List<User> userList) {
-        return userList.stream()
+
+        Set<Category> jobCategories = job.getCategories();
+        Set<Category> relatedCategories = new HashSet<>();
+        Set<User> potentialHelpers;
+
+        for(Category category : jobCategories) {
+            relatedCategories.addAll(category.getListOfRelated());
+        }
+
+        potentialHelpers = userList.stream()
                 .filter(User::isWantsToHelpActive)
                 .filter(user -> user.getStatus().equals(UserStatus.ACTIVE))
                 .filter(helper -> !Collections.disjoint(helper.getCategories(), job.getCategories())) // at least 1 category in common
                 .collect(Collectors.toSet());
+
+        if(potentialHelpers.isEmpty()) {
+            potentialHelpers = userList.stream()
+                    .filter(User::isWantsToHelpActive)
+                    .filter(user -> user.getStatus().equals(UserStatus.ACTIVE))
+                    .filter(helper -> !Collections.disjoint(helper.getCategories(), relatedCategories))
+                    .collect(Collectors.toSet());
+        }
+        return potentialHelpers;
     }
 }
