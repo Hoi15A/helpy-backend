349a44c46132433875240cb6e6c78c3c7a2ff277
Carl Lubojanski <lubojcar@students.zhaw.ch>
2020-12-03 15:14:49 +0100

    Changed return type in Matcher interface
    Co-Authored-By: meletlea <3164+meletlea@users.noreply.github.zhaw.ch>
    Co-Authored-By: Austin Albrecht <2957+albreaus@users.noreply.github.zhaw.ch>
    Co-Authored-By: schwaal3 <3283+schwaal3@users.noreply.github.zhaw.ch>
    Co-Authored-By: pedernin <pedernin@students.zhaw.ch>

--- src/main/java/ch/zhaw/pm3/helpy/matcher/MatcherController.java
+++ src/main/java/ch/zhaw/pm3/helpy/matcher/MatcherController.java
@@ -3,11 +3,11 @@ package ch.zhaw.pm3.helpy.matcher;
 import ch.zhaw.pm3.helpy.model.job.Job;
 import ch.zhaw.pm3.helpy.model.user.User;
 
-import java.util.Set;
+import java.util.*;
 
 public class MatcherController implements Matcher {
     @Override
-    public Set<User> potentialMatches(Job job) {
+    public Collection<User> getPotentialMatches(Job job) {
         return null;
     }
 }
