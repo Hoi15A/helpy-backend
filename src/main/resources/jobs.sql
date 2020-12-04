cdeeb88bf21b8b09ff6ff0cb575644e442adce8e
meletlea <meletlea@students.zhaw.ch>
2020-12-04 14:29:06 +0100

    add due date on existing jobs

--- src/main/resources/jobs.sql
+++ src/main/resources/jobs.sql
@@ -1,18 +1,18 @@
 --
 -- custom test data
 --
-insert into Job (id, created, description, status, title, author_email, matched_helper_email) values (100, '2020-01-01', 'description', 0, 'titel', 'mj@email.com', null);
-insert into Job (id, created, description, status, title, author_email, matched_helper_email) values (101, '2020-01-01', 'description2', 0, 'titel2', 'pepper@potts.com', null);
-insert into Job (id, created, description, status, title, author_email, matched_helper_email) values (102, '2020-01-01', 'description3', 1, 'titel3', 'peggy@america.com', 'captain@america.com');
-insert into Job (id, created, description, status, title, author_email, matched_helper_email) values (103, '2020-01-01', 'description4', 2, 'titel4', 'mj@email.com', 'leandro@email.com');
-insert into Job (id, created, description, status, title, author_email, matched_helper_email) values (104, '2020-01-01', 'description5', 1, 'Ich brauche eine neue Wohnsitzbestätigung.', 'ahmed_miri@gmx.net', 'spidey@email.com');
+insert into Job (id, due_date, created, description, status, title, author_email, matched_helper_email) values (100, '2021-01-01', '2020-01-01', 'description', 0, 'titel', 'mj@email.com', null);
+insert into Job (id, due_date, created, description, status, title, author_email, matched_helper_email) values (101, '2021-01-01', '2020-01-01', 'description2', 0, 'titel2', 'pepper@potts.com', null);
+insert into Job (id, due_date, created, description, status, title, author_email, matched_helper_email) values (102, '2021-01-01', '2020-01-01', 'description3', 1, 'titel3', 'peggy@america.com', 'captain@america.com');
+insert into Job (id, due_date, created, description, status, title, author_email, matched_helper_email) values (103, '2021-01-01', '2020-01-01', 'description4', 2, 'titel4', 'mj@email.com', 'leandro@email.com');
+insert into Job (id, due_date, created, description, status, title, author_email, matched_helper_email) values (104, '2021-01-01', '2020-01-01', 'description5', 1, 'Ich brauche eine neue Wohnsitzbestätigung.', 'ahmed_miri@gmx.net', 'spidey@email.com');
 
-insert into Job (id, created, description, status, title, author_email, matched_helper_email) values (1, '2020-06-16', 'sed justo pellentesque viverra pede ac diam cras pellentesque volutpat dui maecenas tristique est', 0, 'Ich brauche Hilfe ein SBB abo zu Lösen', 'ahmed_miri@gmx.net', null);
-insert into Job (id, created, description, status, title, author_email, matched_helper_email) values (2, '2020-03-16', 'sagittis sapien cum sociis natoque penatibus et magnis dis parturient montes nascetur ridiculus mus', 2, 'Ich brauche hilfe mit meines deutsch aufgaben', 'ahmed_miri@gmx.net', null);
-insert into Job (id, created, description, status, title, author_email, matched_helper_email) values (6, '2020-09-16', 'facilisi cras non velit nec nisi vulputate nonummy maecenas tincidunt lacus at velit vivamus vel', 0, 'Kann mir jemand bei der jobsuche helfen', 'ahmed_miri@gmx.net', null);
+insert into Job (id, due_date, created, description, status, title, author_email, matched_helper_email) values (1, '2021-01-01', '2020-06-16', 'sed justo pellentesque viverra pede ac diam cras pellentesque volutpat dui maecenas tristique est', 0, 'Ich brauche Hilfe ein SBB abo zu Lösen', 'ahmed_miri@gmx.net', null);
+insert into Job (id, due_date, created, description, status, title, author_email, matched_helper_email) values (2, '2021-01-01', '2020-03-16', 'sagittis sapien cum sociis natoque penatibus et magnis dis parturient montes nascetur ridiculus mus', 2, 'Ich brauche hilfe mit meines deutsch aufgaben', 'ahmed_miri@gmx.net', null);
+insert into Job (id, due_date, created, description, status, title, author_email, matched_helper_email) values (6, '2021-01-01', '2020-09-16', 'facilisi cras non velit nec nisi vulputate nonummy maecenas tincidunt lacus at velit vivamus vel', 0, 'Kann mir jemand bei der jobsuche helfen', 'ahmed_miri@gmx.net', null);
 
 ---
-insert into Job (id, created, description, status, title, author_email, matched_helper_email) values (105, '2020-07-30', 'Ich bin der deutschen Sprache nicht mächtig und bräuchte dringend jemanden der mir hilft, mich in meiner Gemeinde anzumelden.', 0, 'Hilfe beim Übersetzen', 'e-lebeau@bluewin.ch', null);
-insert into Job (id, created, description, status, title, author_email, matched_helper_email) values (106, '2020-04-28', 'Ich bin eingeschränkt und kann mich nur im Rollstuhl fortbewegen. Ich bräuchte Hilfe beim Umzug.', 0, 'Hilfe beim Umzug', 'h-fournier@bluewin.ch', null);
-insert into Job (id, created, description, status, title, author_email, matched_helper_email) values (107, '2020-10-01', 'Ich bin blind und muss in nächster Zeit nach Fribourg und brauche Hilfe von jemanden der sich in Fribourg auskennt.', 0, 'Hilfe für Führung in der Stadt Fribourg', 'r-millet@bluewin.ch', null);
+insert into Job (id, due_date, created, description, status, title, author_email, matched_helper_email) values (105, '2021-01-01', '2020-07-30', 'Ich bin der deutschen Sprache nicht mächtig und bräuchte dringend jemanden der mir hilft, mich in meiner Gemeinde anzumelden.', 0, 'Hilfe beim Übersetzen', 'e-lebeau@bluewin.ch', null);
+insert into Job (id, due_date, created, description, status, title, author_email, matched_helper_email) values (106, '2021-01-01', '2020-04-28', 'Ich bin eingeschränkt und kann mich nur im Rollstuhl fortbewegen. Ich bräuchte Hilfe beim Umzug.', 0, 'Hilfe beim Umzug', 'h-fournier@bluewin.ch', null);
+insert into Job (id, due_date, created, description, status, title, author_email, matched_helper_email) values (107, '2021-01-01', '2020-10-01', 'Ich bin blind und muss in nächster Zeit nach Fribourg und brauche Hilfe von jemanden der sich in Fribourg auskennt.', 0, 'Hilfe für Führung in der Stadt Fribourg', 'r-millet@bluewin.ch', null);
 ---
